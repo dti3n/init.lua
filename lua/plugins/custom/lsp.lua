587ac4b1cf06058486d0173a054b59d03571fd78
TengNg <nguyentin148@gmail.com>
2024-12-19 00:47:11 +0700

    feat(blink-cmp): replace cmp plugins with blink-cmp

--- lua/plugins/custom/lsp.lua
+++ lua/plugins/custom/lsp.lua
@@ -4,12 +4,44 @@ return {
     dependencies = {
         { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-        "saadparwaiz1/cmp_luasnip",
+        "saghen/blink.cmp",
     },
     config = function()
-        require("plugins/configs/lsp")
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+        mason_lspconfig.setup_handlers({
+            function(server_name) -- default handler (optional)
+                require("lspconfig")[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+
+            denols = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.denols.setup({
+                    capabilities = capabilities,
+                    root_dir = lspconfig.util.root_pattern(
+                        "deno.json",
+                        "deno.jsonc"
+                    ),
+                })
+            end,
+
+            ts_ls = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.ts_ls.setup({
+                    capabilities = capabilities,
+                    root_dir = lspconfig.util.root_pattern(
+                        ".git",
+                        "tsconfig.json",
+                        "jsconfig.json",
+                        "package.json"
+                    ),
+                    -- single_file_support = false
+                })
+            end,
+        })
     end,
 }
