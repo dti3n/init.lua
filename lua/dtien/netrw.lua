4ea2e7375b7cc8b10ac598ea8b34c0387ab8fe42
TengNg <nguyentin148@gmail.com>
2024-12-21 00:08:22 +0700

    feat: remove lir and back to netrw

--- lua/dtien/netrw.lua
+++ lua/dtien/netrw.lua
@@ -1,30 +1,34 @@
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
 
--- vim.g.netrw_banner = 0
--- vim.g.netrw_altfile = 1
+vim.g.netrw_banner = 0
+vim.g.netrw_altfile = 1
 -- vim.g.netrw_winsize = 30
---
--- vim.keymap.set("n", "-", ":Explore<cr>")
--- vim.keymap.set("n", "<leader>vn", ":Explore<cr>")
--- vim.keymap.set("n", "<leader>vl", ":Lexplore %:p:h<cr>")
---
--- -- these remaps feel pretty good to use
--- -- open file with o and add file with a
--- local autocmd = vim.api.nvim_create_autocmd
--- autocmd("FileType", {
---     pattern = "netrw",
---     desc = "Better mappings for netrw",
---
---     callback = function(desc)
---         local bind = function(lhs, rhs)
---             vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true, silent = true })
---         end
---
---         -- edit new file
---         bind("a", "%")
---
---         -- open file
---         bind("o", "<CR>")
---     end,
--- })
+vim.g.netrw_hide = 0
+
+vim.keymap.set("n", "-", ":Explore<cr>")
+vim.keymap.set("n", "<leader>vn", ":Explore<cr>")
+vim.keymap.set("n", "<leader>vl", ":Lexplore %:p:h<cr>")
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("FileType", {
+    pattern = "netrw",
+    desc = "Better mappings for netrw",
+
+    callback = function(desc)
+        local bind = function(lhs, rhs)
+            vim.keymap.set(
+                "n",
+                lhs,
+                rhs,
+                { remap = true, buffer = true, silent = true }
+            )
+        end
+
+        -- edit new file
+        bind("a", "%")
+
+        -- -- open file
+        -- bind("o", "<CR>")
+    end,
+})
