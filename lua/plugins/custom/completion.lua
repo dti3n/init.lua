b4677e3662cabf65fa865cf2acdac98c1a56218c
TengNg <nguyentin148@gmail.com>
2025-01-08 22:03:53 +0700

    feat(blink): up version + add borders + no-preselect

--- lua/plugins/custom/completion.lua
+++ lua/plugins/custom/completion.lua
@@ -1,15 +1,23 @@
 return {
     "saghen/blink.cmp",
-    version = "v0.*",
+    version = "*",
     opts = {
         keymap = { preset = "enter" },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = "mono",
         },
+        completion = {
+            menu = { border = "single" },
+            documentation = { window = { border = "single" } },
+            accept = { auto_brackets = { enabled = false } },
+            list = { selection = { preselect = false, auto_insert = false } },
+        },
+        signature = { window = { border = "single" } },
         sources = {
-            default = { "lsp", "path", "luasnip", "buffer" },
+            default = { "lsp", "path", "snippets", "buffer" },
             cmdline = {},
         },
+        snippets = { preset = "luasnip" },
     },
 }
