4dca8ba21fdfb2a04a392a6490417388faace53d
TengNg <nguyentin148@gmail.com>
2025-02-14 21:59:00 +0700

    feat: remove blink & back to nvim.cmp

--- lua/plugins/custom/lsp.lua
+++ lua/plugins/custom/lsp.lua
@@ -6,12 +6,23 @@ return {
             -- mason.nvim must be loaded before dependants
             { "williamboman/mason.nvim", config = true },
             "williamboman/mason-lspconfig.nvim",
-            "saghen/blink.cmp",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
         },
         config = function()
+            local cmp = require("cmp")
+            local cmp_lsp = require("cmp_nvim_lsp")
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend(
+                "force",
+                capabilities,
+                cmp_lsp.default_capabilities()
+            )
+
             local mason = require("mason")
             local mason_lspconfig = require("mason-lspconfig")
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
 
             mason_lspconfig.setup_handlers({
                 function(server_name) -- default handler (optional)
@@ -50,6 +61,90 @@ return {
                         },
                     })
                 end,
+            })
+
+            local cmp = require("cmp")
+            local cmp_lsp = require("cmp_nvim_lsp")
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend(
+                "force",
+                capabilities,
+                cmp_lsp.default_capabilities()
+            )
+
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason_lspconfig.setup_handlers({
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+
+                ts_ls = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.ts_ls.setup({
+                        capabilities = capabilities,
+                        root_dir = lspconfig.util.root_pattern(
+                            ".git",
+                            "tsconfig.json",
+                            "jsconfig.json",
+                            "package.json"
+                        ),
+                        -- single_file_support = false
+                    })
+                end,
+
+                tailwindcss = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.tailwindcss.setup({
+                        capabilities = capabilities,
+                        filetypes = {
+                            "html",
+                            "css",
+                            "javascript",
+                            "javascriptreact",
+                            "typescript",
+                            "typescriptreact",
+                            "vue",
+                            "svelte",
+                        },
+                    })
+                end,
+            })
+
+            -- completion ==========
+
+            cmp.setup({
+                -- window = {
+                --     completion = cmp.config.window.bordered(),
+                --     documentation = cmp.config.window.bordered(),
+                -- },
+
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Select,
+                    }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({
+                        behavior = cmp.SelectBehavior.Select,
+                    }),
+                    -- ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+                    -- ['<C-j>'] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete({}),
+                    ["<CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = false,
+                    }),
+                    ["Tab"] = nil,
+                    ["S-Tab"] = nil,
+                }),
+
+                sources = {
+                    { name = "nvim_lsp", max_item_count = 10 },
+                    { name = "path" },
+                },
             })
         end,
     },
